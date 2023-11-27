a32a342ddd22c9a77c459f6705956f0a01b14a33
Azhar <azharnisar006@gmail.com>
2023-11-27 17:37:40 +0500

    hey this is just a begning

---
+++ app/Components/Header.js
@@ -0,0 +1,33 @@
+
+/*
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <>
+
+ <div className='flex justify-between items-center h-16 bg-green-500 px-4'>
+
+<h1>
+
+{props.user}    
+</h1>
+
+<div className='flex gap-4'>
+
+<h3>About</h3>
+<h3>Carear</h3>
+<h3>Blog</h3>
+<h3>{props.edit}</h3>
+
+</div>
+
+ </div>
+
+
+    </>
+  )
+}
+
+export default Header
+*/
